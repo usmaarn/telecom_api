2c8dba40be122b744b199418bd1d24f8592bf1e4
Ezekiel Abodesegun <ezekielphlat@gmail.com>
2025-09-01 13:27:56 +0100

    initial commit

---
+++ src/test/java/dev/usmaarn/telecom/TelecomApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.usmaarn.telecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
